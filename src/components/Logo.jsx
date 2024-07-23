6d4aa4d261d307303958dcbf5c3ee26063c4796b
AnubhavKishore <anubhavkishore023@gmail.com>
2024-07-24 02:58:17 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div class="text-2xl text-white font-bold text-center justify-center p-4 rounded-lg ">
+  TechTalkToday
+</div>
+
+  )
+}
+
+export default Logo
